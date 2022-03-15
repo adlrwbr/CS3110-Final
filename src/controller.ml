430916cabfb93b4da8a5c81cc49dc80221b9966c
Adler Weber <adlerweber8430@gmail.com>
2022-03-15 00:50:17 -0400

    Bootstrap GUI

---
+++ src/controller.ml
@@ -0,0 +1,11 @@
+(** [loop] is the main event loop of the application *)
+let rec loop () =
+    (* wait for next keypress event *)
+    let event = Graphics.wait_next_event [Graphics.Key_pressed] in
+    (* check for quit key else loop again *)
+    if event.key == 'q' then exit 0
+    else loop ()
+
+let start () =
+    let _ = View.init in
+    loop ()
