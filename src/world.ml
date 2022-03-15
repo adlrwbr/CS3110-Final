4e7a620a7bb518ede733aeb9db4baab4f11e8361
axc2@cornell.edu <andrew032012@gmail.com>
2022-03-15 15:58:10 -0400

    implement wt, lt, rt, add, name, category, roads

--- src/world.ml
+++ src/world.ml
@@ -1,24 +1,31 @@
-type wt = string (** TODO *)
-type lt = string (** TODO *)
-type rt = string (** TODO *)
+type wt = { g : Graph.vgt }
 
-let add world name category road pos =
-    raise (Failure "Unimplemented")
+type rt = {
+  coords : (float * float) list;
+  startPt : float * float;
+  endPt : float * float;
+  name : string;
+}
 
-let name world loc =
-    raise (Failure "Unimplemented")
+type lt = {
+  id : int;
+  name : string;
+  category : string;
+  road : rt;
+  pos_on_road : float;
+}
 
-let category world loc =
-    raise (Failure "Unimplemented")
-
-let locations world =
-    raise (Failure "Unimplemented")
+let add world name category road pos = raise (Failure "Unimplemented")
+let name world loc = loc.name
+let category world loc = loc.category
+let locations world = raise (Failure "Unimplemented")
 
 let loc_coord loc =
-    raise (Failure "Unimplemented")
+  ( fst loc.road.startPt
+    +. (loc.pos_on_road *. (fst loc.road.endPt -. fst loc.road.startPt)),
+    snd loc.road.startPt
+    +. (loc.pos_on_road *. (snd loc.road.endPt -. snd loc.road.startPt))
+  )
 
-let roads world =
-    raise (Failure "Unimplemented")
-
-let road_coords road =
-    raise (Failure "Unimplemented")
+let roads world = raise (Failure "Unimplemented")
+let road_coords road = raise (Failure "Unimplemented")
