462164bb5eb8916d69640b4811fedbb3184b9663
axc2@cornell.edu <andrew032012@gmail.com>
2022-03-17 00:32:49 -0400

    changed author file names to netids

--- src/author.mli
+++ src/author.mli
@@ -1,6 +1,6 @@
 (** CS 3110 Spring 2022 Assignment A2 Adventure
 
-    @author Sean Brynjolfsson, Andrew Chen, Adler Weber *)
+    @author smb459, axc2, amw369 *)
 
 val hours_worked : int
 (** [hours_worked] is the number of hours we worked on this assignment. *)
