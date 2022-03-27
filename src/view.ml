a1f15ac1cfaa0c24f26917eed0910bb759464bf5
Adler Weber <adlerweber8430@gmail.com>
2022-03-27 18:02:51 -0400

    Add modal instructions

--- src/view.ml
+++ src/view.ml
@@ -39,11 +39,13 @@ type y_anchor = TOP | MIDDLE | BOTTOM
     to its coordinates *)
 type anchor = y_anchor * x_anchor
 
-(** [draw_text x y anchor text] draws the string [text] with its [anchor] at
-    pixel-space coordinate ([x], [y]) *)
-let draw_text (x : int) (y : int) (anchor : anchor) (text : string) : unit =
-  set_text_size 100; (* TODO: why isn't this working? *)
-  (* set_font "-misc-dejavu sans mono-bold-r-normal--256-0-0-0-m-0-iso8859-1"; *)
+(** [draw_text x y anchor size text] draws the string [text] with its [anchor]
+    at pixel-space coordinate ([x], [y]) in the font size [size] *)
+let draw_text (x : int) (y : int) (anchor : anchor) ?size:(font_size : int = 12)
+  (text : string) : unit =
+  (* set_font "-misc-dejavu sans mono-bold-r-normal--12-0-0-0-m-0-iso8859-1"; *)
+  "-misc-dejavu sans mono-bold-r-normal--" ^ Int.to_string font_size
+  ^ "-0-0-0-m-0-iso8859-1" |> set_font;
   moveto x y;
   let width, height = text_size text in
   let dy =
@@ -67,9 +69,9 @@ let draw_loc (loc : World.lt) =
   (* draw node *)
   fill_circle x y 15;
   (* draw name label *)
-  loc |> World.name |> draw_text (x - 15) (y + 20) (BOTTOM, LEFT);
+  loc |> World.name |> draw_text (x) (y + 15) (BOTTOM, CENTER);
   (* draw category label *)
-  loc |> World.category |> draw_text (x - 20) (y - 30) (BOTTOM, LEFT)
+  loc |> World.category |> draw_text (x) (y - 15) (TOP, CENTER)
 
 (** [draw_road loc] draws the location [loc] *)
 let draw_road (road : Road.t) =
@@ -81,11 +83,13 @@ let draw_road (road : Road.t) =
   lineto x2 y2;
   (* draw name label *)
   let x, y = World.midpt road |> world_to_pixel in
-  road |> Road.name |> draw_text x y (BOTTOM, LEFT)
+  road |> Road.name |> draw_text x y (BOTTOM, CENTER)
 
 let draw world =
   let _ = List.map draw_loc (World.locations world) in
   let _ = List.map draw_road (World.roads world) in
+  draw_text 5 (size_y () - 5) (TOP, LEFT) "Press q to quit";
+  draw_text 5 (size_y () - 25) (TOP, LEFT) "Press e to edit world";
   ()
 
 let draw_input_popup prompt input =
@@ -106,4 +110,15 @@ let draw_input_popup prompt input =
   input |> draw_text (box_ll_x + 35) (size_y () / 2 - 30) (BOTTOM, LEFT)
 
 let draw_edit_mode () =
-  draw_text 0 0 (BOTTOM, LEFT) "Welcome to edit mode"
+  (* draw welcome text *)
+  let title = "Welcome to edit mode." in
+  draw_text 5 (size_y ()) (TOP, LEFT) ~size:15 title;
+  let _, title_height = text_size title in
+  (* draw keybinding guide *)
+  let keybind_height = "P" |> text_size |> snd in
+  draw_text 5 (size_y () - title_height - 1 * (keybind_height + 5)) (TOP, LEFT)
+    "Press r to place road endpoints";
+  draw_text 5 (size_y () - title_height - 2 * (keybind_height + 5)) (TOP, LEFT)
+    "Press l to place a location";
+  draw_text 5 (size_y () - title_height - 3 * (keybind_height + 5)) (TOP, LEFT)
+    "Press e to exit"
