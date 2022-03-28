d921ad67614d8b2c117b48063c2ff2be83ee321a
Sean <smbrynjolfson@gmail.com>
2022-03-28 02:56:09 -0400

    Add bfs test (looks to be working), add neighbors test.

--- test/main.ml
+++ test/main.ml
@@ -1,6 +1,7 @@
 open OUnit2
 open Pathfinder
 open Graph
+open Algo
 
 let pullout added_graph = match added_graph with | (x,y) -> y
 
@@ -11,13 +12,18 @@ let my_graph = empty |> ap |> ap
 let verifiable = connect 1 2 my_graph
 let unverifiable = connect 3 4 (my_graph |> ap |> ap)
 
-let transitive_one_to_five = connect 
+let one_five_cycle = empty |> ap |> ap |> ap |> ap |> ap
+|> connect 1 2 |> connect 2 3 |> connect 3 4 |> connect 4 5
 
 
 (** Cumulative test suite. *)
 let graph_tests = "graph test suite" >::: [
     "graph which is verifiable" >:: (fun _ -> let _ = verify verifiable in ());
-    "graph which is not verifiable" >:: (fun _ -> assert_raises (InvalidGraph) (fun () -> verify unverifiable))
+    "graph which is not verifiable" >:: (fun _ -> assert_raises (InvalidGraph) 
+    (fun () -> verify unverifiable));
+    "connect to 1-3" >:: (fun _ -> assert_equal (neighbors one_five_cycle 2) [1;3]);
+    "bfs" >:: (fun _ -> assert_equal (bfs one_five_cycle 3) [2;1;3;4;5]);
+
 ]
 
 let _ = run_test_tt_main graph_tests
