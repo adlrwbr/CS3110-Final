382939702c96ab46daf5141a4bb476178d1954ce
axc2@cornell.edu <andrew032012@gmail.com>
2022-04-24 02:04:37 -0400

    implement nextdoor_neighbors

--- src/road.ml
+++ src/road.ml
@@ -30,33 +30,37 @@ let inter_coords road1 road2 : (float * float) option =
       (* calculate the slopes of lines 1 and 2 *)
       let m1 = Algo.slope start1_x start1_y end1_x end1_y in
       let m2 = Algo.slope start2_x start2_y end2_x end2_y in
-      (* x and y is the intersection of the two lines extended from line 
+      (* x and y is the intersection of the two lines extended from line
          segments 1 and 2 *)
-      (* for now, we are saying that it is impossible for two roads with the
-         same slope to intersect *)
+      (* for now, we are saying that it is impossible for two roads with
+         the same slope to intersect *)
       if m1 = m2 then None
       else
-      let x =
-        (start2_y -. start1_y +. (m1 *. start1_x) -. (m2 *. start2_x))
-        /. (m1 -. m2)
-      in
-      let y = (m2 *. (x -. start2_x)) +. start2_y in
-      (* Check if the intersection point is actually on the segments. *)
-      if
-        Algo.in_range x start1_x end1_x
-        && Algo.in_range x start2_x end2_x
-        && Algo.in_range y start1_y end1_y
-        && Algo.in_range y start2_y end2_y
-      then Some (x, y)
-      else None
+        let x =
+          (start2_y -. start1_y +. (m1 *. start1_x) -. (m2 *. start2_x))
+          /. (m1 -. m2)
+        in
+        let y = (m2 *. (x -. start2_x)) +. start2_y in
+        (* Check if the intersection point is actually on the
+           segments. *)
+        if
+          Algo.in_range x start1_x end1_x
+          && Algo.in_range x start2_x end2_x
+          && Algo.in_range y start1_y end1_y
+          && Algo.in_range y start2_y end2_y
+        then Some (x, y)
+        else None
 
 let intersection (r1 : t) (r2 : t) : it option =
   match inter_coords r1 r2 with
   | None -> None
-  | Some intersect -> 
+  | Some intersect ->
       let r1_dist = Algo.distance r1.startPt r1.endPt in
-      let pos_on_road1 = Algo.distance intersect r1.startPt /. r1_dist in
+      let pos_on_road1 =
+        Algo.distance intersect r1.startPt /. r1_dist
+      in
       let r2_dist = Algo.distance r2.startPt r2.endPt in
-      let pos_on_road2 = Algo.distance intersect r2.startPt /. r2_dist in
-        Some
-        { road1 = r1; road2 = r2; pos_on_road1 = pos_on_road1; pos_on_road2 = pos_on_road2 }
+      let pos_on_road2 =
+        Algo.distance intersect r2.startPt /. r2_dist
+      in
+      Some { road1 = r1; road2 = r2; pos_on_road1; pos_on_road2 }
